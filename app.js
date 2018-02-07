baf7098d6ddfc6b32dff8537b64828f214041b54
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-07 09:05:30 +0100

    Using 3rd party modules

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
 console.log('Starting app.js');
 
-const errorManage = (error) => {
-  if (error) {
-    console.log('Unable to write to file');
-  } else {
-    console.log('Done file');
-  }
-};
-const res = notes.addNote();
-console.log(res);
-console.log(`Result 12 plus 3 is: ${notes.add(12, 3)}`);
+// console.log(_.isString(true));
+// console.log(_.isString('Alberto'));
+const filteredArray = _.uniq(['Alberto', 1, 'Alberto', 1, 2, 3, 4]);
+console.log(`Array filtered: ${filteredArray}`);
+console.log(filteredArray);
+
+// const errorManage = (error) => {
+//   if (error) {
+//     console.log('Unable to write to file');
+//   } else {
+//     console.log('Done file');
+//   }
+// };
+// const res = notes.addNote();
+// console.log(res);
+// console.log(`Result 12 plus 3 is: ${notes.add(12, 3)}`);
 
 
 // const user = os.userInfo();

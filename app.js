dd3599e59f4850d6d3b770ae849eaae63935d549
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-07 16:04:55 +0100

    lesson 13
    Getting input from user

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes');
+
+const command = process.argv[2];
+console.log('Command:', command);
+console.log(process.argv);
+
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading a note');
+} else if (command === 'remove') {
+  console.log('Remove a note');
+} else {
+  console.log('Command not recognize');
+}
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
